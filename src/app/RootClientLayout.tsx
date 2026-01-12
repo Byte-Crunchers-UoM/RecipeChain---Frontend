37c7f3b886fdcf5ce975e71eb92debe7d9aaea49
AmalViduranga <amalvidu20@gmail.com>
2026-01-13 03:48:40 +0530

    refactor: remove Web3AuthProvider and related context, update layout components

--- src/app/RootClientLayout.tsx
+++ src/app/RootClientLayout.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import { Web3AuthProvider } from '@/app/lib/web3/Web3AuthProvider';
-
 export default function RootClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <Web3AuthProvider>
+    <>
       {children}
-    </Web3AuthProvider>
+    </>
   );
 }
--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import { Web3AuthContextProvider } from '@/contexts/Web3AuthContext';
-
 export default function ProfileLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <Web3AuthContextProvider>
+    <>
       {children}
-    </Web3AuthContextProvider>
+    </>
   );
 }
